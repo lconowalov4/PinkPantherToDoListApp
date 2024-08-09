1f58538a27eba07909187e2e248b13e03851e532
lconowalov4 <rw6lfe@gmail.com>
2024-08-09 15:37:47 +0300

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import Create from './Create'
+
+const Home = () => {
+    const [todos, setTodos] = useState([])
+    return (
+        <div className='home'>
+           <h2>Pink Panther ToDo List</h2> 
+           <Create />
+           {
+                todos.length === 0 
+                ?
+                <div>
+                    <h2>No Record</h2>
+                </div>
+                :
+                todos.map(todo =>  (
+                    <div>
+                        {todo}
+                    </div>
+                ))
+           }
+        </div>
+    )
+}
+
+export default Home
